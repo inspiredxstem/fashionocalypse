7e048a7c63d4be5cacdcd26e1b1c7bd8fa982e19
tehdslayer <srosario2018@bklawtech.com>
2016-05-21 20:34:57 +0000

    checkpoint

--- script/State.js
+++ script/State.js
@@ -5,6 +5,6 @@ TopDownGame.game = new Phaser.Game(720, 480, Phaser.AUTO, 'GameScreen');
 TopDownGame.game.state.add('Boot', TopDownGame.Boot);
 TopDownGame.game.state.add('Preload', TopDownGame.Preload);
 TopDownGame.game.state.add('Game', TopDownGame.Game);
-TopDownGame.game.state.add('Death', TopDownGame.Death);
+//TopDownGame.game.state.add('Death', TopDownGame.Death);
 
 TopDownGame.game.state.start('Boot');
